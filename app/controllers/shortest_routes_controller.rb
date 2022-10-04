dbcddee439b0aaa5f4a11d47fc15008717709a12
samuel <sbacon803@gmail.com>
2022-10-04 07:51:04 -0600

    add destinations search for local data

--- app/controllers/shortest_routes_controller.rb
+++ app/controllers/shortest_routes_controller.rb
@@ -1,6 +1,6 @@
 class ShortestRoutesController < ApplicationController
   def index
-    response = HTTP.get("https://api.mapbox.com/directions-matrix/v1/mapbox/driving/-122.418563,37.751659;-122.422969,37.75529;-122.426904,37.759617?sources=0&annotations=distance,duration&access_token=#{Rails.application.credentials.mapbox_api_key}")
+    response = HTTP.get("https://api.mapbox.com/directions-matrix/v1/mapbox/driving/-105.07044,39.66747;-104.837684,39.65612;-105.06748859825893,39.65969794446177;-105.289425,39.929021?sources=1&annotations=distance,duration&access_token=#{Rails.application.credentials.mapbox_api_key}")
     render json: response.parse
   end
 end
